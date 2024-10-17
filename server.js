3b3be24302863cd9d7d83ed5c10b1e383db473f6
RefreshInnovates <refreshinnovates@gmail.com>
2024-10-17 01:03:51 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Stripe = require('stripe');
+const app = express();
+
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post('/connection_token', async (req, res) => {
+  try {
+    const token = await stripe.terminal.connectionTokens.create();
+    res.json({ secret: token.secret });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
